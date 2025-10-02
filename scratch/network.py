85d8f1666a8729eb337617c6ba6e9ac9ebd40bb9
FARHAN SHAIKH <farhan.shaikh199704@gmail.com>
2025-10-02 11:46:02 +0200

    Solutions updated for 1 and 2

--- scratch/network.py
+++ scratch/network.py
@@ -45,7 +45,26 @@ class Network():
 
         The method should return the output of the network.
         '''
-        pass
+        params=self.params
+
+        # Layer 1
+        z1=np.dot(params['W1'], x_train.T)
+        a1=self.activation_func(z1)
+
+        # Layer 2
+        z2=np.dot(params['W2'], a1)
+        a2=self.activation_func(z2)
+
+        #Layer 3
+        z3=np.dot(params['W3'], a2)
+        a3=self.output_func(z3)
+
+        #Final output
+        output={"x":x_train,
+                "z1": z1, "a1":a1,
+                "z2": z2, "a2":a2,
+                "z3":z3, "a3":a3}
+        return a3, output
 
 
     def _backward_pass(self, y_train, output):
@@ -55,14 +74,35 @@ class Network():
         The method should return a dictionary of the weight gradients which are used to update the weights in self._update_weights().
 
         '''
-        pass
+        m=y_train.shape[0]
+        params=self.params
+
+        x_train=output["x"]
+        a1,a2,a3=output["a1"], output["a2"], output["a3"]
+        z1,z2,z3= output["z1"], output["z2"], output["z3"]
+
+        #Output layer
+        dz3=(a3-y_train.T)*self.output_func_deriv
+        dW3=(1/m)*np.dot(dz3,a2.T)
+
+        #Hidden Layer 2
+        dz2=np.dot(params['W3'].T, dz3)* self.activation_func_deriv(z2)
+        dW2 = (1/m) * np.dot(dz2, a1.T)
+
+        # Hidden layer 1
+        dz1 = np.dot(params['W2'].T, dz2) * self.activation_func_deriv(z1)
+        dW1 = (1/m) * np.dot(dz1, x_train)
+        
+        weights_gradient = {"dW1": dW1, "dW2": dW2, "dW3": dW3}
+        return weights_gradient
 
 
     def _update_weights(self, weights_gradient, learning_rate):
         '''
         TODO: Update the network weights according to stochastic gradient descent.
         '''
-        pass
+        for l in [1,2,3]:
+            self.params[f'W{l}'] -= self.learning_rate * grads[f'dW{l}']
 
 
     def _print_learning_progress(self, start_time, iteration, x_train, y_train, x_val, y_val):
@@ -90,7 +130,8 @@ class Network():
         TODO: Implement the prediction making of the network.
         The method should return the index of the most likeliest output class.
         '''
-        pass
+        a3, _=self._forward_pass(x)
+        return np.argmax(a3, axis=0)
 
 
 
@@ -114,3 +155,13 @@ class Network():
                 self._update_weights(weights_gradient, learning_rate=learning_rate)
 
             self._print_learning_progress(start_time, iteration, x_train, y_train, x_val, y_val)
+
+if __name__ == "__main__":
+    from scratch.network import Network
+    import numpy as np
+
+    net = Network([784, 128, 64, 10])
+    X = np.random.randn(5, 784)  # dummy batch of 5 images
+    out, _ = net._forward_pass(X)
+    print("Forward output shape:", out.shape)
+
--- scratch/res_network.py
+++ scratch/res_network.py
@@ -10,8 +10,11 @@ class ResNetwork(Network):
         TODO: Initialize the class inheriting from scratch.network.Network.
         The method should check whether the residual network is properly initialized.
         '''
-        pass
-        
+        super().__init__(sizes, epochs, learning_rate, random_state)
+
+        # Check if residual connection is properly initialised
+        if sizes[1] != sizes[2]:
+            raise ValueError("Residual connection requires hidden_layer_1 and hidden_layer_2 to have the same size")
         
 
 
@@ -20,7 +23,25 @@ class ResNetwork(Network):
         TODO: Implement the forward propagation algorithm.
         The method should return the output of the network.
         '''
-        pass
+        params = self.params
+        
+        # Layer 1
+        z1 = np.dot(params['W1'], x_train.T)
+        a1 = self.activation_func(z1)
+        
+        # Layer 2 with residual connection
+        z2 = np.dot(params['W2'], a1)
+        a2 = self.activation_func(z2) + a1   # residual skip
+        
+        # Output layer
+        z3 = np.dot(params['W3'], a2)
+        a3 = self.output_func(z3)
+
+        output = {"x":x_train,
+                  "z1": z1, "a1": a1,
+                  "z2": z2, "a2": a2,
+                  "z3": z3, "a3": a3}
+        return a3, output
 
 
 
@@ -31,6 +52,33 @@ class ResNetwork(Network):
         The method should also account for the residual connection in the hidden layer.
 
         '''
-        pass
+        m=y_train.shape[0]
+        params=self.params
+        output=output
+
+        x_train=output["x"]
+        a1, a2, a3=output["a1"], output["a2"], output["a3"]
+        z1, z2, z3 = output["z1"], output["z2"], output["z3"]
+
+        #Output error
+        dz3 = (a3 - y_train.T) * self.output_func_deriv(z3)
+        dW3 = (1/m) * np.dot(dz3, a2.T)
+
+        # Hidden layer 2
+        dz2 = np.dot(params['W3'].T, dz3) * self.activation_func_deriv(z2)
+        dW2 = (1/m) * np.dot(dz2, a1.T)
+
+        # Hidden layer 1 
+        # normal path: through W2
+        dh1_from_W2 = np.dot(params['W2'].T, dz2)
+        dh1_residual = np.dot(params['W3'].T, dz3)
+        dh1_total = dh1_from_W2 + dh1_residual
+
+        dz1 = dh1_total * self.activation_func_deriv(z1)
+        dW1 = (1/m) * np.dot(dz1, x_train)
+        
+        grads = {"dW1": dW1, "dW2": dW2, "dW3": dW3}
+        return grads
+
 
 
